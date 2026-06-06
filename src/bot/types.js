ff9d523c976414a66e9a25c2097000de3d483cef
NoNeed1252 <closecombatchimps@icloud.com>
2026-06-06 12:46:19 -0400

    Baseline sync from gross-bros-v7 commit c984426b6b92c0c34004ee872f23a412a38cbc76

---
+++ src/bot/types.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} Asset
+ * @property {string} currency - Currency code (e.g., 'XRP', 'USD', or hex)
+ * @property {string} [issuer] - Issuer address (omit for XRP)
+ * @property {string} [value] - Amount value
+ */
+
+module.exports = {};
